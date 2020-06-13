5634816a0e3bec9079e4fd0fd261f7d293035dc1
Baibhav <baibhab@tcs.com>
2020-06-13 16:30:03 +0530

    structured

--- urlshort/urlshort.py
+++ urlshort/urlshort.py
@@ -1,17 +1,16 @@
-from flask import Flask, render_template, request, url_for, redirect, flash, abort, session, jsonify
+from flask import render_template, request, url_for, redirect, flash, abort, session, jsonify, Blueprint
 import json
 import os.path as path
 from werkzeug.utils import secure_filename
 
-app = Flask(__name__)
-app.secret_key="awdjawd2r3r3d"
+bp = Blueprint('urlshort', __name__)
 
-@app.route("/")
+@bp.route("/")
 def home():
     return render_template("home.html", codes=session.keys())
 
 
-@app.route("/short-url", methods=["GET", "POST"])
+@bp.route("/short-url", methods=["GET", "POST"])
 def user():
     short_url = request.form['code']
     if request.method == 'POST':
@@ -22,26 +21,26 @@ def user():
         if short_url in urls.keys():
             # print('same')
             flash("The short name already exists. Pick something else.")
-            return redirect(url_for('home'))
+            return redirect(url_for('urlshort.home'))
         if 'url' in request.form.keys():
             urls[short_url] = {'url': request.form['url']}
         else:
             f = request.files['file']
             full_name = short_url + secure_filename(f.filename)
-            f.save('I:/Projects/url-shortener/static/user_files/' + full_name)
+            f.save('I:/Projects/url-shortener/urlshort/static/user_files/' + full_name)
             urls[short_url] = {'file':full_name}
         with open('urls.json', 'w') as url_file:
             json.dump(urls, url_file)
             session[short_url] = True
         return render_template('short-url.html', code=short_url, serverurl='http://localhost:5000/')
     else:
-        return redirect(url_for('home'))
+        return redirect(url_for('urlshort.home'))
 
-@app.route('/api/session')
+@bp.route('/api/session')
 def session_keys():
     return jsonify(list(session.keys()))
 
-@app.route('/<string:code>')
+@bp.route('/<string:code>')
 def short_path(code):
     urls = {}
     if path.exists('urls.json'):
@@ -54,7 +53,7 @@ def short_path(code):
                     return redirect(url_for('static', filename='user_files/' + urls[code]['file']))
     abort(404)
 
-@app.errorhandler(404)
+@bp.errorhandler(404)
 def page_not_found(error):
     return render_template('notfound.html', serverurl="http://localhost:5000"), 404
 
